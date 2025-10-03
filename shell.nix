0f37a26c6495ea0a99e613bde5318a4f7d28a845
Felipe Marcelino <felipe.marcelino1991@gmail.com>
2025-10-03 19:06:46 -0300

    requirements

--- shell.nix
+++ shell.nix
@@ -9,7 +9,7 @@ let
     inherit (pkgs) system;
     config.allowUnfree = true;
   };
-  pythonPackages = pkgs-unfree.python313Packages;
+  pythonPackages = pkgs-unfree.python312Packages;
 in
 pkgs-unfree.mkShell {
   buildInputs = [
